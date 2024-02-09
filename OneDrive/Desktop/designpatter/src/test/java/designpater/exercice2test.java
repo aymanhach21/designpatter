303031f746146478d0c95b36852afd9701ed9ca7
aymanhach <aymanhach21@gmail.com>
2024-02-09 22:02:10 +0100

    exercice 2 strategie*

---
+++ OneDrive/Desktop/designpatter/src/test/java/designpater/exercice2test.java
@@ -0,0 +1,21 @@
+package designpater;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class exercice2test {
+
+    @Test
+    public void testValidation() {
+        Validation validation = new Validation();
+        assertEquals("valider", validation.resultatexam(),
+                "La méthode resultatexam de Validation devrait retourner 'valider'");
+    }
+
+    @Test
+    public void testNonValidation() {
+        Nonvalider nonValidation = new Nonvalider();
+        assertEquals("non valider", nonValidation.resultatexam(),
+                "La méthode resultatexam de Non_valider devrait retourner 'non valider'");
+    }
+}
